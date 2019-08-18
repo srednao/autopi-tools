ea66a479a0f3af31011d30d43dfbbe802881b459
srednao <anders.odelberg@gmail.com>
2019-08-18 18:02:37 +0200

    Update charge to telegram Ioniq working (GOM included).py
    GOM calculation now based on air temp.

--- charge to telegram Ioniq working (GOM included).py
+++ charge to telegram Ioniq working (GOM included).py
@@ -24,7 +24,7 @@ def checkChargeStatus():
     charging = get_charging()
     if charging == 0 or charging == -1:
         if persistance['charging'] == True:
-            bot_sendtext("Charging stopped. Last known State of charge "+format(persistance['SOC'],'.1f')+"% BMS")
+            bot_sendtext("Charging stopped. Last known State of charge "+format(persistance['SOC'],'.1f')+"% (BMS)")
             #bot_sendtext("Charging stopped. Last known State of charge "+format(persistance['SOC'],'.1f')+"% BMS - "+format(soc_display,'.1f')+ "% Display")
             persistance['charging'] = False
             save(persistance)
@@ -35,47 +35,23 @@ def checkChargeStatus():
     soc = get_soc()
     soc_display = get_soc_display()
     temp = get_temp()
+    air_temp = get_air_temp()
+
+    # if batt temp is more or equal to 25 then set to 25
+    #
+    #if temp >= 25:
+        #temp = 25
 
     # alert if just started to charge
     #
     if persistance['charging'] == False:
-        bot_sendtext("Charging started at a rate of "+format(2*chargingPower,'.2f')+"kW. State of charge now "+format(soc,'.1f')+"% BMS - "+format(soc_display,'.1f')+ "% Display GOM now "+format(0.01*soc_display*2800/(16.08813 + (temp*(-0.13873))),'.0f')+"km")
+        bot_sendtext("Charging started at a rate of "+format(2*chargingPower,'.2f')+"kW. SOC BMS = "+format(soc,'.1f')+"%, SOC Display = "+format(soc_display,'.1f')+ "%, GOM "+format(0.01*soc_display*2800/(16.08813 + (air_temp*(-0.13873))),'.0f')+"km, batt. temp. = "+format(temp,'.0f')+" Degrees, air temp. = "+format(air_temp,'.0f')+" Degrees")
 
 
-    # 40% alert
+    # Alerts at 40, 50, 60, 70, 80, 90 and 94% SOC BMS
     #
-    if soc >= 40 and persistance['SOC'] < 40:
-        bot_sendtext("Charging now at a rate of "+format(2*chargingPower,'.2f')+"kW. State of charge now "+format(soc,'.1f')+"% BMS - "+format(soc_display,'.1f')+ "% Display GOM now "+format(0.01*soc_display*2800/(16.08813 + (temp*(-0.13873))),'.0f')+"km")
-
-    # 50% alert
-    #
-    if soc >= 50 and persistance['SOC'] < 50:
-        bot_sendtext("Charging now at a rate of "+format(2*chargingPower,'.2f')+"kW. State of charge now "+format(soc,'.1f')+"% BMS - "+format(soc_display,'.1f')+ "% Display GOM now "+format(0.01*soc_display*2800/(16.08813 + (temp*(-0.13873))),'.0f')+"km")
-
-    # 60% alert
-    #
-    if soc >= 60 and persistance['SOC'] < 60:
-        bot_sendtext("Charging now at a rate of "+format(2*chargingPower,'.2f')+"kW. State of charge now "+format(soc,'.1f')+"% BMS - "+format(soc_display,'.1f')+ "% Display GOM now "+format(0.01*soc_display*2800/(16.08813 + (temp*(-0.13873))),'.0f')+"km")
-
-    # 70% alert
-    #
-    if soc >= 70 and persistance['SOC'] < 70:
-        bot_sendtext("Charging now at a rate of "+format(2*chargingPower,'.2f')+"kW. State of charge now "+format(soc,'.1f')+"% BMS - "+format(soc_display,'.1f')+ "% Display GOM now "+format(0.01*soc_display*2800/(16.08813 + (temp*(-0.13873))),'.0f')+"km")
-
-    # 80% alert
-    #
-    if soc >= 80 and persistance['SOC'] < 80:
-        bot_sendtext("Charging now at a rate of "+format(2*chargingPower,'.2f')+"kW. State of charge now "+format(soc,'.1f')+"% BMS - "+format(soc_display,'.1f')+ "% Display GOM now "+format(0.01*soc_display*2800/(16.08813 + (temp*(-0.13873))),'.0f')+"km")
-
-   # 90% alert
-    #
-    if soc >= 90 and persistance['SOC'] < 90:
-        bot_sendtext("Charging now at a rate of "+format(2*chargingPower,'.2f')+"kW. State of charge now "+format(soc,'.1f')+"% BMS - "+format(soc_display,'.1f')+ "% Display GOM now "+format(0.01*soc_display*2800/(16.08813 + (temp*(-0.13873))),'.0f')+"km")
-
-    # 100% alert ... not sure if this can really happen
-    #
-    if soc >= 94 and persistance['SOC'] < 94:
-        bot_sendtext("Charging now at a rate of "+format(2*chargingPower,'.2f')+"kW. State of charge now "+format(soc,'.1f')+"% BMS - "+format(soc_display,'.1f')+ "% Display GOM now "+format(0.01*soc_display*2800/(16.08813 + (temp*(-0.13873))),'.0f')+"km")
+    if (soc >= 40 and persistance['SOC'] < 40) or (soc >= 50 and persistance['SOC'] < 50) or (soc >= 60 and persistance['SOC'] < 60) or (soc >= 70 and persistance['SOC'] < 70) or (soc >= 80 and persistance['SOC'] < 80) or (soc >= 90 and persistance['SOC'] < 90) or (soc >= 94 and persistance['SOC'] < 94):
+        bot_sendtext("Charging at a rate of "+format(2*chargingPower,'.2f')+"kW. SOC BMS = "+format(soc,'.1f')+"%, SOC Display = "+format(soc_display,'.1f')+ "%, GOM "+format(0.01*soc_display*2800/(16.08813 + (air_temp*(-0.13873))),'.0f')+"km, batt. temp. = "+format(temp,'.0f')+" Degrees, air temp. = "+format(air_temp,'.0f')+" Degrees")
 
     # store status for next time
     #
@@ -84,8 +60,10 @@ def checkChargeStatus():
     save(persistance)
 
     #return {"msg": "Charging at "+format(chargingPower,'.2f')+"kW, SOC now "+format(soc,'.1f')+"% "+format(temp,'.1f')+" temp"}
-    #return {"msg": "Charging at "+format(chargingPower,'.2f')+"kW, SOC now "+format(soc,'.1f')+"% "+format(0.01*soc*2800/(16.08813 + (temp*(-0.13873))),'.1f')+" km"}
-    return {"msg": "Charging at "+format(chargingPower,'.2f')+"kW, SOC (BMS) now "+format(soc,'.1f')+"% GOM now "+format(0.01*soc_display*2800/(16.08813 + (temp*(-0.13873))),'.0f')+"km"}
+    #return {"msg": "Charging at "+format(chargingPower,'.2f')+"kW, SOC (BMS) now "+format(soc,'.1f')+"% GOM now "+format(0.01*soc_display*2800/(16.08813 + (temp*(-0.13873))),'.0f')+"km and batt. temp. is "+format(temp,'.0f')+" Degrees Centigrade"}
+    #return {"msg": "Charging at "+format(chargingPower,'.2f')+"kW, SOC (BMS) = "+format(soc,'.1f')+"% GOM = "+format(0.01*soc_display*2800/(16.08813 + (air_temp*(-0.13873))),'.0f')+" km,  batt. temp.="+format(temp,'.0f')+" Degrees, air temp. = "+format(air_temp,'.0f')+" Degrees"}
+    return {"msg": "Charging at a rate of "+format(2*chargingPower,'.2f')+"kW. SOC BMS = "+format(soc,'.1f')+"%, SOC Display = "+format(soc_display,'.1f')+ "%, GOM "+format(0.01*soc_display*2800/(16.08813 + (air_temp*(-0.13873))),'.0f')+"km, batt. temp. = "+format(temp,'.0f')+" Degrees, air temp. = "+format(air_temp,'.0f')+" Degrees"}
+
 
 # send message to telegram
 #
@@ -127,6 +105,23 @@ def get_temp():
         'header': '7E4',
         'baudrate': 500000,
         'formula': 'twos_comp(bytes_to_int(message.data[17:18]),8)',
+        'protocol': '6',
+        'verify': False,
+        'force': True,
+        }
+    return __salt__['obd.query'](*args, **kwargs)['value']
+
+# get Air Temperature, used to calcualte GOM range
+#
+def get_air_temp():
+    args = ['air_temp']
+    kwargs = {
+        'mode': '21',
+        'pid': '80',
+        'header': '7E6',
+        'baudrate': 500000,
+        #'formula': '(twos_comp(bytes_to_int(message.data[15:16]), 8)-80)/2',
+        'formula': '(bytes_to_int(message.data[14:15])-80)/2',
         'protocol': '6',
         'verify': False,
         'force': True,
@@ -223,7 +218,7 @@ def get_charging():
     except:
         return -1
 
-# Weird response, always seem to be true
+# Charging CCS
 def get_charging_chademo():
 #  try:
         args = ['CCS Plug']
@@ -239,7 +234,7 @@ def get_charging_chademo():
         }
         return (int(__salt__['obd.query'](*args, **kwargs)['value'])&64)/64
 
-# Weird response, always seem to be true
+# Charging 230V
 def get_charging_normal():
 #  try:
         args = ['J1772 Plug']
